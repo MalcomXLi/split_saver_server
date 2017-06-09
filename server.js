d904270228e715c7d1ac2024d02943eba7e2a41e
Malcom Li <malcom309@gmail.com>
2017-06-09 11:53:34 -0400

    initial commit to server

---
+++ server.js
@@ -0,0 +1,13 @@
+const express = require('express');
+const app = express();
+
+app.get('/', function (req, res) {
+   console.log("Got a GET request for the homepage");
+   res.send('Hello GET');
+})
+
+var server = app.listen(8081, () => {
+    const port = server.address().port;
+    console.log(server.address());
+    console.log("Server is listening at http://localhost:%s", port);
+});
