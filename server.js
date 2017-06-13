14b591b0791264d6e181f06107d64d28827ee579
Malcom Li <malcom309@gmail.com>
2017-06-13 11:58:11 -0400

    Set up port better

--- server.js
+++ server.js
@@ -6,7 +6,7 @@ app.get('/', function (req, res) {
    res.send('Hello GET');
 })
 
-var server = app.listen(8081, () => {
+var server = app.listen(process.env.PORT || 8081, () => {
     const port = server.address().port;
     console.log(server.address());
     console.log("Server is listening at http://localhost:%s", port);
